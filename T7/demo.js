b3cf20a2fdf48fdcfe8e2dc455e609e993f489ad
JanyanP <janyan.rodriguez.271@gmail.com>
2020-11-29 22:39:41 -0600

    Agregar archivos de proyecto.

---
+++ T7/demo.js
@@ -0,0 +1,62 @@
+﻿/// <reference path="Scripts/angular.js" />
+
+//import { error } from "jquery";
+//var MyApp = angular.module("MyApp", ['ngRoute', 'AlumnoService']);
+var MyApp = angular.module("MyApp", ['ngRoute']);
+
+MyApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider
+            .when('/Añadir', {
+                templateUrl: 'Vista/añadir.html',
+                controller: 'AddController'
+            })
+            .when('/Home', {
+                templateUrl: 'Vista/home.html',
+                controller: 'HomeController'
+            })
+            .otherwise({
+                redirectTo: '/Home'
+            });
+
+    }]);
+    
+
+MyApp.controller("AddController", function ($scope, AlumApi) {
+   // $scope.message = "en Añadir";
+    /*$scope.addAlumno = function () {
+        var alumniToAdd = {
+            'Nombre': $scope.name,
+            'Edad': $scope.age,
+            'Carrera': $scope.carrera,
+            'Promedio': $scope.calif
+        };*/
+        AlumApi.AddAlumno()
+            .success(function (response) {
+                alert("alumno añadido");
+                /*$scope.name = undefined;
+                $scope.age = undefined;
+                $scope.carrera = undefined;
+                $scope.calif = undefined;*/
+            }).
+            error(function (response) {
+                alert("error al añadir");
+            });
+    }
+
+});
+
+
+MyApp.controller("HomeController", function ($scope, AlumApi) {
+    //$scope.message = "en Home";
+    /*
+    function getAlumnos() {
+        AlumApi.getAlumnos().success(function (almns) {
+            $scope.almns = almns;
+        })
+            .error(function (error) {
+                $scope.status = 'Unable to load almns data' + error.message;
+            })
+    }*/
+
+});
---
+++ T7/service.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="cookies.js" />
+/// <reference path="demo.js" />
+/// <reference path="scripts/angular.js" />
+var AlumnoService = angular.module('AlumnoService', []);
+
+AlumnoService.factory('AlumApi', function () {
+    //var urlBase = "https://localhost:44300/api";
+    var AlumApi = {};
+    /*AlumApi.getAlumnos = function () {
+        //return $http.get(urlBase + '/Alumnos');
+
+    };
+    */
+    AlumApi.AddAlumno = function () {
+        cargarCookies();
+        /*
+        setCookie("nombreAlumno", alum.Nombre, 10);
+        setCookie("edadAlumno", alum.Edad, 10);
+        setCookie("carreraAlumno", alum.Carrera, 10);
+        setCookie("promedioAlumno", alum.Promedio, 10);*/
+        //return $http.post(urlBase + '/Alumnos/', alum);
+    };
+
+
+
+    return AlumApi;
+});
